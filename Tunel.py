1b314c8d1aaa22f9b82937e0365aca9482aaeb41
Reykinou <79137567+Reykinou@users.noreply.github.com>
2021-02-22 10:23:34 +0100

    Update Tunel.py

--- Tunel.py
+++ Tunel.py
@@ -6,35 +6,35 @@
 
                             #Geometría del Túnel
 
-X=9.4 #ancho tunel
-Y=80.0 #largo del tunel
-Z=6.0 #altura tunel
-cx=0.2 #tamaño celda en x
-cy=0.3 #tamaño celda en y
-cz=0.2 #tamaño celda en z
+X=9.4 #tunnel width
+Y=80.0 #tunnel length
+Z=6.0 #tunnel height 
+cx=0.2 #cell size in x
+cy=0.3 #cell size in y
+cz=0.2 #cell size z
 
-#Curva para dibujar tunel - interpolación Excel con geometría AutoCad
+#Tunnel geometry - Excel interpolation
 
 def f(x):
     return -1*0.00037731654713724100*x**6 + 0.01064032662972640000*x**5 - 0.12023951690048300000*x**4 + 0.69353748265166400000*x**3 - 2.25803586101028000000*x**2 + 4.43075769525603000000*x**1 + 0.00018151820924572300
 
                             #Inputs Correa
 
-b = 2 #ancho de correa
-h = 1.3 #altura de la correa
-x_correa = 2.5 #distancia desde pared del tunel
+b = 2 #conveyor belt width
+h = 1.3 #conveyor belt height 
+x_correa = 2.5 #lateral distance to tunnel walls
 
-                            #Incendio de diseño (nada aún)
+                            #Design Fire 
 
-vel_viento = 2.0
-spread_rate = 0.02
+vel_viento = 2.0  #wind speed
+spread_rate = 0.02 #flame spread rate 
 
 #----------------------------CÁLCULOS PREVIOS-----------------------------------
 #Cálculos varios
 
-n_x=X/cx #numero de celdas en X
-n_y=Y/cy #numero de celdas en Y
-n_z=Z/cz #numero de celdas en Z
+n_x=X/cx #number of cells in X
+n_y=Y/cy #number of cells in Y
+n_z=Z/cz #number of cells in Z
 
 
 
@@ -78,24 +78,22 @@ with open(Tunel,'w') as file_object:
 
     file_object.write("Correa \n")
     file_object.write("\n")
-    file_object.write("&OBST XB="+str(x_correa)+","+str(x_correa+b)+",0,"+str(Y)+",0,"+str(h)+" / \n") #Obstaculo para correa
+    file_object.write("&OBST XB="+str(x_correa)+","+str(x_correa+b)+",0,"+str(Y)+",0,"+str(h)+" / \n") #Conveyor belt obstacule
     file_object.write("\n")
 
     n=1
     dx=cx
     while n<=47:
-        file_object.write("&OBST XB="+str((n-1)*dx)+","+str(n*dx)+",0,"+str(Y)+","+str(1.4+f(n*dx))+",6 SURF_ID='CONCRETE SURFACE' / \n") #Para dibujar forma del túnel
+        file_object.write("&OBST XB="+str((n-1)*dx)+","+str(n*dx)+",0,"+str(Y)+","+str(1.4+f(n*dx))+",6 SURF_ID='CONCRETE SURFACE' / \n") #Tunnel shape drawing
         n+=1
 
-    # file_object.write("\n")
-    # file_object.write("&WIND V0=8.0/ \n") #VIENTO - NO SÉ SI DEFINIRLO ASÍ
 
     file_object.write("\n")
     file_object.write("&SURF ID = 'CONCRETE SURFACE', MATL_ID = 'CONCRETE', RGB = 128,128,128, THICKNESS = 0.4/ \n")
     file_object.write("&MATL ID = 'CONCRETE', SPECIFIC_HEAT = 0.88, DENSITY = 2100., CONDUCTIVITY = 1.0 / \n")
     file_object.write("\n")
 
-                #INCENDIO SOLO PARA PROBAR - NO ES EL DE DISEÑO
+                #option B design fire
 
     file_object.write("&VENT XB=2.5,4.5,5.0,30.0,1.3,1.3 SURF_ID='FIRE' / \n") #Área de 2x2 sbre la correa
     file_object.write("&REAC FUEL = 'PROPANE', SOOT_YIELD = 0.15, CO_YIELD = 0.06, HEAT_OF_COMBUSTION = 30000. / \n")
